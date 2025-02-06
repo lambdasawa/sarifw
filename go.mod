187e6bff4e95250199dace4fcde6020e71a86664
Tsubasa Irisawa <lambdasawa@gmail.com>
2025-02-07 05:27:42 +0900

    Open .sarif file by code command

--- go.mod
+++ go.mod
@@ -2,9 +2,7 @@ module github.com/lambdasawa/sarifw
 
 go 1.23.5
 
-require github.com/spf13/cobra v1.8.1
-
 require (
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/caarlos0/env/v6 v6.10.1
+	github.com/mattn/go-shellwords v1.0.12
 )
--- main.go
+++ main.go
@@ -4,28 +4,68 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
 
+	"github.com/caarlos0/env/v6"
 	astgrep "github.com/lambdasawa/sarifw/pkg/ast-grep"
 	"github.com/lambdasawa/sarifw/pkg/ripgrep"
+	"github.com/mattn/go-shellwords"
 )
 
+type Config struct {
+	OutputDir string `env:"SARIFW_OUTPUT_DIR" envDefault:".sarifw/"`
+	Editor    string `env:"SARIFW_EDITOR" envDefault:"code"`
+}
+
 func main() {
-	if err := run(); err != nil {
+	var cfg Config
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := run(cfg); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func run() error {
-	stdout, err := execAndConvert()
+func run(cfg Config) error {
+	sarif, err := execute()
 	if err != nil {
 		return err
 	}
-	fmt.Println(stdout)
+
+	if err := os.MkdirAll(filepath.Dir(cfg.OutputDir), 0755); err != nil {
+		return err
+	}
+
+	tmp, err := os.CreateTemp(cfg.OutputDir, "*.sarif")
+	if err != nil {
+		return err
+	}
+	log.Printf("tmp: %s", tmp.Name())
+
+	if err := os.WriteFile(tmp.Name(), []byte(sarif), 0644); err != nil {
+		return err
+	}
+
+	editorCmd, err := shellwords.Parse(cfg.Editor)
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command(editorCmd[0], append(editorCmd[1:], tmp.Name())...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
 
 	return nil
 }
 
-func execAndConvert() (string, error) {
+func execute() (string, error) {
 	osArgs := os.Args[1:]
 	name := osArgs[0]
 
